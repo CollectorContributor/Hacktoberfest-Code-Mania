3917bc1feceae0de43d29830902edca9ba3ba308
decent learner <92971853+decentlearner@users.noreply.github.com>
2021-10-22 12:57:06 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include<iostream>    // including iostream header file
+using namespace std;
+// Driver code
+int main(){
+  int a, b; // variable declaration of integer type.
+  cin>>a>>b;  // taking numbers.
+  cout<<"sum = "<<a+b;  //printing sum of a and b.
+  return o;  
+}

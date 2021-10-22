dbbffc6e65e528d8349a8ba9a61f648ea4e79d59
decent learner <92971853+decentlearner@users.noreply.github.com>
2021-10-22 22:28:22 +0530

    Create subtract.cpp

---
+++ CPP/subtract.cpp
@@ -0,0 +1,9 @@
+#include<iostream>  // including header file
+using namespace std;
+// Driver code
+int main(){
+  int a, b;   // allocating memory for storing two integers.
+  cin>>a>>b;  // taking input two integers.
+  cout<<"Difference = "<<a-b;   // Printing output on console.
+  return 0;
+}
